a1fbc0f68a96828e4b2ab2e0c2480d2f9b54fa80
Jacek Swierk <jacek.swierk@gmail.com>
2020-09-20 11:05:08 +0200

    emacs: add initial native configuration

---
+++ home/config/emacs/early-init.el
@@ -0,0 +1,46 @@
+;;; early-init.el --- Pre-initialisation config -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; Emacs 27+ introduces early-init.el, which is run before init.el, before
+;; package and UI initialization happens.
+
+;;; Code:
+
+;; In Emacs 27+, package initialization occurs before `user-init-file' is
+;; loaded, but after `early-init-file' - disable this behaviour as we use
+;; `straight'.
+(setq package-enable-at-startup nil)
+
+;; Prefix declarations are unneeded bulk added to our autoloads file. Best we
+;; don't have to deal with them at all.
+(setq autoload-compute-prefixes nil)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+;; Set frame size to be slightly larger and always create frames in top left
+;; corner of the screen.
+(setq default-frame-alist '((menu-bar-lines . 0)
+                            (tool-bar-lines . 0)
+                            (vertical-scroll-bars)
+                            (top . 0)
+                            (left . 0)
+                            (user-size . t)
+                            (width . 100)
+                            (height . 50)))
+
+;; Set initial frame to be maximized
+; (setq initial-frame-alist '((fullscreen . maximized)))
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Make sure not to load an out-of-date init.elc file.
+(setq load-prefer-newer t)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; early-init.el ends here
