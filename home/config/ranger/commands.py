2d13cc48d1ea4c928d91d21efe69f157abeeb7e5
Jacek Swierk <jacek.swierk@gmail.com>
2022-04-09 12:18:29 +0200

    ranger: update custom commands

--- home/config/ranger/commands.py
+++ home/config/ranger/commands.py
@@ -1,51 +1,93 @@
 from ranger.api.commands import Command
 
+class toggle_flat(Command):
+    """
+    :toggle_flat
+
+    Flattens or unflattens the directory view.
+    """
+
+    def execute(self):
+        if self.fm.thisdir.flat == 0:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = -1
+            self.fm.thisdir.load_content()
+        else:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = 0
+            self.fm.thisdir.load_content()
+
+
 class z(Command):
-  """
-  :z
+    """
+    :z
 
-  Jump to directory using fasd
-  """
-  def execute(self):
-    args = self.rest(1).split()
-    if args:
-      directories = self._get_directories(*args)
-      if directories:
-        self.fm.cd(directories[0])
-      else:
-        self.fm.notify("No results from fasd", bad=True)
+    Jump to directory using fasd.
+    """
+    def execute(self):
+        args = self.rest(1).split()
+        if args:
+            directories = self._get_directories(*args)
+            if directories:
+                self.fm.cd(directories[0])
+            else:
+                self.fm.notify("No results from fasd", bad=True)
 
-  def tab(self, tabnum: int):
-    start, current = self.start(1), self.rest(1)
-    for path in self._get_directories(*current.split()):
-      yield start + path
+    def tab(self, tabnum: int):
+        start, current = self.start(1), self.rest(1)
+        for path in self._get_directories(*current.split()):
+            yield start + path
 
-  @staticmethod
-  def _get_directories(*args):
-    import subprocess
-    output = subprocess.check_output(["fasd", "-dl", *args],
-                                     universal_newlines=True)
-    return output.strip().split("\n")
+    @staticmethod
+    def _get_directories(*args):
+        import subprocess
+        output = subprocess.check_output(["fasd", "-dl"] + list(args),
+                                         universal_newlines=True)
+        dirs = output.strip().split("\n")
+        dirs.sort(reverse=True)  # Listed in ascending frecency
+        return dirs
+
+
+class z_dir(Command):
+    """
+    :z_dir
+
+    Jump interactively to directory using fasd.
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        fzf = self.fm.execute_command("fasd -dl | fzf 2>/dev/tty",
+                                      universal_newlines=True,
+                                      stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
+        if fzf.returncode == 0:
+            selected = os.path.abspath(stdout.strip())
+            if os.path.isdir(selected):
+                self.fm.cd(selected)
+            else:
+                self.fm.select_file(selected)
+
 
 class fzf(Command):
-  """
-  :fzf
+    """
+    :fzf
 
-  Find a file using fzf.
+    Find a file using fzf.
 
-  With a prefix argument select only directories.
+    With a prefix argument select only directories.
 
-  See: https://github.com/junegunn/fzf
-  """
-  def execute(self):
-    import subprocess
-    import os.path
-    fzf = self.fm.execute_command("fzf", universal_newlines=True,
-                                  stdout=subprocess.PIPE)
-    stdout, stderr = fzf.communicate()
-    if fzf.returncode == 0:
-      fzf_file = os.path.abspath(stdout.rstrip('\n'))
-      if os.path.isdir(fzf_file):
-        self.fm.cd(fzf_file)
-      else:
-        self.fm.select_file(fzf_file)
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        fzf = self.fm.execute_command("fzf --no-multi", universal_newlines=True,
+                                      stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
+        if fzf.returncode == 0:
+            selected = os.path.abspath(stdout.strip())
+            if os.path.isdir(selected):
+                self.fm.cd(selected)
+            else:
+                self.fm.select_file(selected)
