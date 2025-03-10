a88a2f2602dcf4a96e459cfd969448478d2692a1
Jacek Swierk <jswierk@akamai.com>
2025-03-10 07:47:06 +0100

    emacs: adapt to macos

--- home/config/emacs/init.el
+++ home/config/emacs/init.el
@@ -587,6 +587,8 @@ anything that can be a key's definition."
 (use-package! kkp
   :init
 
+  (setq kkp-terminal-query-timeout 1)
+
   (global-kkp-mode +1))
 
 ;;;; Encryption
@@ -1848,6 +1850,9 @@ possibly new window."
     (if arg
         (switch-to-buffer-other-window (current-buffer))
       (switch-to-buffer (current-buffer)))))
+
+(bind-key "<home>" #'move-beginning-of-line)
+(bind-key "<end>"  #'move-end-of-line)
 
 (set-leader-keys!
   "b m" #'switch-to-messages-buffer
@@ -4545,8 +4550,10 @@ Restore the buffer with \\<dired-mode-map>`\\[revert-buffer]'."
   ;;   - append indicator (one of */=>@|) to entries
   ;;   - print sizes like 1K 234M 2G etc
   ;;   - group directories before files
-  (setopt dired-listing-switches
-          "-AlvFh --group-directories-first --time-style=long-iso")
+  (if (string= system-type "darwin")
+      (setopt dired-listing-switches "-AlvFh")
+    (setopt dired-listing-switches
+            "-AlvFh --group-directories-first --time-style=long-iso"))
 
   ;; Just show 'Dired' in mode line.
   (setopt dired-switches-in-mode-line 0)
@@ -4568,6 +4575,10 @@ Restore the buffer with \\<dired-mode-map>`\\[revert-buffer]'."
 
   ;; Hide free space label at the top
   (setopt dired-free-space nil)
+
+  ;; On MacOS, ls doesn't support --dired option.
+  (when (string= system-type "darwin")
+    (setq dired-use-ls-dired nil))
 
   ;; Compress with Zstandard by default
   (setopt dired-compress-file-default-suffix ".zst"
