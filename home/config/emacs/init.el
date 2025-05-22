0a375245b51c38f8e599eb0af3612d42ebb66916
Jacek Swierk <jacek.swierk@gmail.com>
2025-05-22 21:11:17 +0200

    emacs: further increase read-process-output-max

--- home/config/emacs/init.el
+++ home/config/emacs/init.el
@@ -3849,7 +3849,7 @@ defeats the purpose of `corfu-sort-function'."
   ;; Increase the amount of data which Emacs reads from the process. The Emacs
   ;; default is too low (4k) considering that the some of the language server
   ;; responses are in 800k - 3M range.
-  (setq read-process-output-max (* 1024 1024))
+  (setq read-process-output-max (* 1024 1024 4))
 
   ;; Inhibit logging a JSONRPC-related events.
   (fset #'jsonrpc--log-event #'ignore)
