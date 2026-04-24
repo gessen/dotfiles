aeb98392f2c0b53dbefdfbf3a142170bef58362e
Jacek Swierk <jswierk@akamai.com>
2026-04-24 10:53:10 +0200

    ssh: do not autostart ssh-agent

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -760,20 +760,6 @@ end
 
 ### SSH
 
-# Start SSH agent
-if ! pgrep -u $USER ssh-agent >/dev/null
-    ssh-agent -c -a $XDG_RUNTIME_DIR/ssh_agent \
-        | sed "s|^echo|#echo|" >/tmp/ssh-agent.fish
-end
-
-if test -z $SSH_AGENT_PID
-    source "/tmp/ssh-agent.fish"
-end
-
-# Add SSH key
-ssh-add -l | grep -q (ssh-keygen -lf $HOME/.ssh/id_rsa | awk '{print $2}')
-or ssh-add
-
 # Copy current terminfo file to the given host
 abbr -a ssh-copy-terminfo --set-cursor \
     infocmp -a '|' ssh % tic -x -o ~/.terminfo /dev/stdin
