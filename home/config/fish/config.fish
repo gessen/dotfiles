f88acafe0b7681d95a7004ef23ab43a5ff8d6b57
Jacek Swierk <jacek.swierk@gmail.com>
2026-09-13 09:26:01 +0200

    --wip-- diffnav: add config

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -341,6 +341,7 @@ abbr -a gdct git describe --tags '(git rev-list --tags --max-count=1)'
 
 abbr -a gd git diff
 abbr -a gd2 git -c delta.side-by-side=true diff
+abbr -a gdn git -c pager.diff=diffnav diff
 abbr -a gds git diff --staged
 abbr -a gds2 git -c delta.side-by-side=true diff --staged
 abbr -a gdt git -c diff.external=difft diff
