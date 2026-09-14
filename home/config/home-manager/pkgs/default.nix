d7ffc60504fd808ecc9591e1e93c75b9c6391b9b
Jacek Swierk <jswierk@akamai.com>
2026-09-14 09:45:31 +0200

    nix: set up edgeworkers buildenv

--- home/config/home-manager/pkgs/default.nix
+++ home/config/home-manager/pkgs/default.nix
@@ -3,6 +3,8 @@
 with pkgs;
 
 {
+  cross-completion = callPackage ./cross-completion { };
+  ew-buildenv = callPackage ./ew-buildenv { };
   gn = callPackage ./gn { };
   osc52 = callPackage ./osc52 { };
 }
